ecbdcb0ecfe1ca3471b64f94edddfb6d47087347
william <william.del.rosario@iceshop.nl>
2019-07-20 21:56:50 +0200

    defining an events map

--- src/models/User.ts
+++ src/models/User.ts
@@ -12,17 +12,5 @@ const rootUrl = 'http://localhost:3000/users';
 export class User {
     public events: Eventing = new Eventing();
     public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-
-    constructor(
-        private data: UserProps
-    ) {}
-
-    get(propName: string): (number | string) {
-        return this.data[propName];
-    }
-
-    set(update: UserProps): void {
-        Object.assign(this.data, update)
-    }
 }
 
