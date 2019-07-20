4b0ddf19859ca13fa1cd0a7ee4f033ad5a222df3
william <william.del.rosario@iceshop.nl>
2019-07-20 19:24:44 +0200

    more complicated extractions

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
 import { User } from "./models/User";
 
-const user = new User({name: 'newrecord', age: 90});
-
-user.events.on('change', () => {
-    console.log('change');
-});
-
-user.events.trigger('change');
-
-user.save();
+const user = new User({name: 'newrecord1', age: 90});
