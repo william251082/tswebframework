1d0028cf86491efba0c6c6166eb62cd52ea1b4a4
william <william.del.rosario@iceshop.nl>
2019-07-20 14:25:42 +0200

    webframework initialized

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import {User} from "./models/User";
+
+const user = new User({name: 'john', age: 20});
+
+
+console.log(user.get('name'));
+console.log(user.get('age'));
+
+console.log(user.set({name:'doe', age: 21}));
