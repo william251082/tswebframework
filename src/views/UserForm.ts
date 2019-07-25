04ca105f7a2b590efd11ee7f4c6121901d4c849e
william <william.del.rosario@iceshop.nl>
2019-07-25 22:11:39 +0200

    binding event handlers

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -4,8 +4,13 @@ export class UserForm {
 
     eventsMap(): { [key: string]: () => void } {
         return {
-            'click:button': this.onButtonClick
+            'click:button': this.onButtonClick,
+            'mouseenter:h1': this.onHeaderHover
         }
+    }
+
+    onHeaderHover(): void {
+        console.log('h1 hover')
     }
 
     onButtonClick(): void {
@@ -24,9 +29,24 @@ export class UserForm {
         `;
     }
 
+    bindEvents(fragment: DocumentFragment): void {
+        const eventsMap = this.eventsMap();
+
+        for (let eventKey in eventsMap) {
+            const [eventName, selector] = eventKey.split(':');
+
+            fragment.querySelectorAll(selector).forEach(element => {
+                    addEventListener(eventName, eventsMap[eventKey]);
+                }
+            )
+        }
+    }
+
     render(): void {
         const templateElement = document.createElement('template');
         templateElement.innerHTML = this.template();
+
+        this.bindEvents(templateElement.content);
 
         this.parent.append(templateElement.content);
     }
