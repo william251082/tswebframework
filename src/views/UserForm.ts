a5bfc5c08d4a73e72b8fb0f244129afe241a8b86
william <william.del.rosario@iceshop.nl>
2019-07-25 11:07:54 +0200

    building user form

---
+++ src/views/UserForm.ts
@@ -0,0 +1,13 @@
+export class UserForm {
+    parent: Element
+
+    template(): string {
+        return `
+            <div>
+                <h1>   
+                    User
+                </h1>   
+            </div>   
+        `;
+    }
+}
