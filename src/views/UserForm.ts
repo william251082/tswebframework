7379120a7a3aca8551b6028140375012491b62dd
william <william.del.rosario@iceshop.nl>
2019-07-26 07:41:41 +0200

    reading input text

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -6,31 +6,40 @@ export class UserForm {
     }
 
     bindModel(): void {
-            this.model.on('change', () => {
+        this.model.on('change', () => {
             this.render();
         });
     }
 
     eventsMap(): { [key: string]: () => void } {
         return {
+            'click:.set-name': this.onSetNameClick,
             'click:.set-age': this.onSetAgeClick
         }
     }
 
+    onSetNameClick = (): void => {
+        const input = this.parent.querySelector('input');
+
+        const name = input.value;
+
+        this.model.set({name})
+    };
+
     onSetAgeClick = (): void => {
         this.model.setRandomAge();
-    }
+    };
 
     template(): string {
         return `
             <div>
                 <h1>   
-                    User
+                    User Form
                 </h1>   
                 <div>User name: ${this.model.get('name')}</div>
                 <div>User age: ${this.model.get('age')}</div>
                 <input type="text">
-                <button>Click Me</button>
+                <button class="set-name">Change Name</button>
                 <button class="set-age">Set Random Age</button>
             </div>   
         `;
