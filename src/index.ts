8448c79fd5c13a4e6a11b79eb25091525712b318
william <william.del.rosario@iceshop.nl>
2019-07-28 20:30:39 +0200

    testing region mapping

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import {UserForm} from "./views/UserForm";
+import {UserEdit} from "./views/UserEdit";
 import {User} from "./models/User";
 
 const user = User.buildUser({name: 'doutz', age: 20});
 const root = document.getElementById('root');
 
 if (root) {
-    const useForm = new UserForm(root, user);
-    useForm.render();
+    const userEdit = new UserEdit(root, user);
+    userEdit.render();
+    console.log(userEdit);
 } else {
     throw new Error('Root element not found');
 }
